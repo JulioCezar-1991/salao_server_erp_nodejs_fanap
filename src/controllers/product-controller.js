abf0ce3989919dcf0cf3e60e587de8efd6b1ffb1
juliocezar_1991 <juliocezar_1991@hotmail.com>
2019-10-10 17:36:02 -0300

    Inclusao relacionamento entre tables

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -11,13 +11,16 @@ exports.postLogin = (req, res, next) => {
 
 
 exports.postCreateLogin = (req, res, ne) => {
+    const empresa = req.query.empresa.substring(0, 150);
     const name = req.query.name.substring(0, 150);
     const login = req.query.login.substring(0, 150);
     const password = req.query.password.substring(0, 11);
     const telefone = req.query.telefone.substring(0, 11);
     const email = req.query.email.substring(0, 11);
     console.log(req.query);
-    queryCreateUser(`INSERT INTO mydb.usuario(usuario.name, usuario.login, usuario.password) VALUES('${name}','${login}','${password}')`, `INSERT INTO mydb.contato(contato.telefone, contato.email, id_usuario) VALUES('${telefone}','${email}', (select usuario.id_usuario from mydb.usuario where usuario.id_usuario = last_insert_id()));`, res);
+    queryCreateUser(`INSERT INTO mydb.usuario(usuario.name, usuario.login, usuario.password) VALUES('${name}','${login}','${password}')`,
+        `INSERT INTO mydb.contato(contato.telefone, contato.email, id_usuario) VALUES('${telefone}','${email}', (select usuario.id_usuario from mydb.usuario where usuario.id_usuario = last_insert_id()));`,
+        `INSERT INTO mydb.empresa(empresa.empresa, empresa.id_usuario) VALUES('${empresa}', (select max(usuario.id_usuario) from mydb.usuario));`, res);
 };
 
 /* exports.postCreateLogin = (req, res) => {
@@ -67,7 +70,7 @@ function execSQLQuery(sqlQry, res) {
     });
 }
 
-function queryCreateUser(sqlUser, sqlCon, res) {
+function queryCreateUser(sqlUser, sqlCon, sqlEmp, res) {
     const connection = mysql.createConnection({
         host: "localhost",
         port: "3306",
@@ -77,16 +80,21 @@ function queryCreateUser(sqlUser, sqlCon, res) {
     });
     connection.query(sqlUser, function (error) {
         if (error)
-            console.log('executou um erro!');
+            console.log('executou um erro user!');
         else
-        return connection.query(sqlCon, function (error, results, fields) {
-            if (error)
-                console.log('executou um erro!');
-            else
-                res.json(results);
-            connection.end();
-            console.log('execução bem sucedida !');
-        });
+            return connection.query(sqlCon, function (error, results, fields) {
+                if (error)
+                    console.log('executou um erro con!');
+                else
+                    return connection.query(sqlEmp, function (error, results, fields) {
+                        if (error)
+                            console.log('executou um erro emp!');
+                        else
+                            res.json(results);
+                        connection.end();
+                        console.log('execução bem sucedida !');
+                    });
+            });
     });
 }
 
